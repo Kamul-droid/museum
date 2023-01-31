b8a43370a8a7de1aa29055dba74724fabbd868c7
Lacheese <81234033+Lacheese@users.noreply.github.com>
2023-01-31 22:33:35 +0400

    test card

--- src/Containers/main/Pages/card.js
+++ src/Containers/main/Pages/card.js
@@ -5,20 +5,20 @@ const Card = (props) => {
     const {img, title, credit, id} = props;
 
     return (
-        <div className="col-lg-4 col-md-6 col-12 mb-4 mb-lg-0">
+        <div data-testid = 'popular-object' className="col-lg-4 col-md-6 col-12 mb-4 mb-lg-0">
             <div className="custom-block-wrap">
-                <img src={img} className="custom-block-image img-fluid" alt="" width={150} height={150}  />
+                <img data-testid = 'image-object' src={img} className="custom-block-image img-fluid" alt="" width={150} height={150}  />
 
                 <div className="custom-block">
-                    <div className="custom-block-body">
+                    <div data-testid = 'title-object' className="custom-block-body">
                         <h5 className="mb-3">{title}</h5>
 
-                        <p>{credit}</p>
+                        <p data-testid = 'credit-object'>{credit}</p>
 
                                                 
                     </div>
 
-                    <Link to={"/details/"+id+"/true/"} className="custom-btn btn">Details</Link>
+                    <Link data-testid = 'details-object' to={"/details/"+id+"/true/"} className="custom-btn btn">Details</Link>
                 </div>
             </div>
             
